4d320addaf723cb8b4ab66355399d293c4e89629
kirsten tay <taykirsten11@gmail.com>
2021-07-04 16:48:24 +0000

    updated the  Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,12 +7,12 @@ const Hero=(Hero)=>{
     return(
         <Fragment>
     
-            <div className="flex flex-row flex-1 justify-around items-center  space-x-10">
-            <div className="flex flex-col justify-center items-center  transform rotate-90 ... border-2 p-5 border-gray-200">
+            <div className="flex flex-row flex-1 justify-around items-center    space-x-10">
+            <div className="flex flex-col  justify-center items-center mx-60  transform rotate-90 ...border-2 p-5 border-gray-200">
                 <img src={img8} class=""/>
                 </div>
 
-             <div className="flex flex-col justify-center  py-3 px-40  items-center">
+             <div className="flex flex-col justify-center  text-3xl  items-center">
                         <span>ABOUT US</span>
                         <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
                             <span> Enim eum dolor voluptatum. Totam nam optio veritatis ad distinctio itaque dolor corporis, fugit a eius sapiente quas eveniet aliquid, quidem nesciunt?</span>
