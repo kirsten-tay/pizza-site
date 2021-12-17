d61a6bc033f19875b13e711dc66eb61128d9f176
Kirsten tay <taykirsten11@gmail.com>
2021-12-17 19:33:50 +0000

    updated the PROJECT

--- src/components/Carouselcomponent.jsx
+++ src/components/Carouselcomponent.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import  Carousel from 'react-responsive-carousel';
 
-import { motion } from 'framer-motion'
-
+ 
 
 const variants = {
     hidden: { x: -200 },
